6d53588794ed5b3a3ad75d00ab1ff726182bfeb4
DELL <DELL@DELL>
2023-04-18 15:53:35 +0530

    First Comit

---
+++ src/CharacterDemo.java
@@ -0,0 +1,15 @@
+
+public class CharacterDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c1='A';
+		char ch = 65;
+		System.out.println(c1);
+		System.out.println(ch);
+		
+		
+
+	}
+
+}
---
+++ src/DataTypesDemo.java
@@ -0,0 +1,28 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1000000000;
+		byte byt=100;
+		short shottt=10000;
+		long longgg=1000000000;
+		float floatt=10000.5555555555555555f;
+		double doublee=10000.555555555555d;
+		boolean bolean=true;
+		char ch1 ='B';
+		String strinng ="yogesh"; 
+		System.out.println(num );
+		System.out.println(byt );
+		System.out.println(shottt );
+		System.out.println(longgg);
+		System.out.println(floatt);
+		System.out.println(doublee);
+		System.out.println(bolean );
+		System.out.println(ch1);
+		System.out.println(strinng );
+		
+
+	}
+
+}
